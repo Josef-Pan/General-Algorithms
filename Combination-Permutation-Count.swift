83d09e19f352b25df09c59cb044927d36ac397fb
Josef <panjunfeng@hotmail.com>
2022-03-29 22:11:59 +1000

    Update Combination-Permutation-Count.swift

--- Combination-Permutation-Count.swift
+++ Combination-Permutation-Count.swift
@@ -18,3 +18,22 @@ func Combinations(_ total: Int, _ selection: Int)->Int{
     return C[total][selection]
 }
 
+// P(n, k) = P(n-1, k) + k* P(n-1, k-1)
+// P(5,3) = 5x4x3 = P(4,3) + 3 * P(4,2)
+// 5x4x3 = 2*  4*3   + 3 * 4*3
+func Permutations(_ total: Int, _ selection: Int)->Int{
+    var P:[[Int]] = Array(repeating: Array(repeating: 0, count: selection+1), count: total+1)
+    for i in 0..<total+1{
+        for j in 0..<(min(i, selection) + 1){
+            if j == 0 {
+                P[i][j] = 1
+            }else{
+                P[i][j] = P[i - 1][j] + (j * P[i - 1][j - 1]) // P(n, k) = P(n-1, k) + k* P(n-1, k-1)
+            }
+            if (j < selection) {
+                P[i][j + 1] = 0
+            }
+        }
+    }
+    return P[total][selection]
+}
